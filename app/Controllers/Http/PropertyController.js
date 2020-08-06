183ab3e6ffa7a3df8eb0b7371f460eef9127e5fd
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-08-05 21:49:19 -0300

    listagem, busca e exclusão de imóvel

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Property = use('App/Models/Property')
+
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
@@ -17,7 +19,10 @@ class PropertyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index () {
+    const properties = Property.all()
+
+    return properties
   }
 
   /**
@@ -40,7 +45,12 @@ class PropertyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
   }
 
   /**
@@ -62,7 +72,14 @@ class PropertyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params, auth, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id != auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await property.delete()
   }
 }
 
