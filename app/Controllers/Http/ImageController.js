383d16f466ed22e8e1ba9e616e77e107c1fd4cce
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-08-23 15:22:39 -0300

    controller de imagem

---
+++ app/Controllers/Http/ImageController.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Image = use('App/Models/Image')
+const Property = use('App/Models/Propery')
+
+class ImageController {
+
+    async store ({ request }) {
+        
+    }
+
+}
+
+module.exports = ImageController
